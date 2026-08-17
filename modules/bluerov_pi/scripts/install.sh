4e267c886308c983c28cf37be121ec1f45ee7dd2
Evan Palmer <evanp922@gmail.com>
2026-08-17 14:33:24 -0700

    update bluerov pi install script

--- modules/bluerov_pi/scripts/install.sh
+++ modules/bluerov_pi/scripts/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Install apt packages
 sudo apt-get update \
   && sudo apt-get install -y \
     python3-pip \
@@ -12,15 +11,16 @@ sudo apt-get update \
     git \
   && sudo apt-get autoremove -y
 
-# Clone the bluerov-deployment repo
 cd ~ \
-  && git clone git@github.com:evan-palmer/bluerov-deployment.git
+  && git clone git@github.com:Robotic-Decision-Making-Lab/RDML_BlueROV2_Deployment.git
 
-# Configure the default gateway for the Ethernet interface
-sudo cp /home/pi/bluerov-development/bluerov-pi/network/eth0.network /etc/systemd/network/ \
+# configure the default gateway for the Ethernet interface
+sudo cp ~/RDML_BlueROV2_Deployment/modules/bluerov_pi/network/eth0.network /etc/systemd/network/ \
   && sudo systemctl restart systemd-networkd
 
-# Setup the ROS systemd service
-sudo cp /home/pi/bluerov-development/bluerov-pi/systemd/ros.service /etc/systemd/system/ \
+# setup the ROS systemd service
+#
+# this runs by default so that MAVROS doesn't need to be launched manually
+sudo cp ~/RDML_BlueROV2_Deployment/modules/bluerov_pi/services/ros.service /etc/systemd/system/ \
   && sudo systemctl enable ros.service \
   && sudo systemctl start ros.service
