b735b91a48230ef2bb930976db718848209edcd2
Evan Palmer <evanp922@gmail.com>
2026-09-08 14:45:00 -0700

    fix permissions

--- modules/bluerov_pi/scripts/nat.sh
+++ modules/bluerov_pi/scripts/nat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # configure the network to point to the topside device for internet access
-nmcli device modify eth0 ipv4.addresses 192.168.2.2/24 ipv4.gateway 192.168.2.1 ipv4.dns 8.8.8.8 ipv4.method manual
+sudo nmcli device modify eth0 ipv4.addresses 192.168.2.2/24 ipv4.gateway 192.168.2.1 ipv4.dns 8.8.8.8 ipv4.method manual
 
 # force the default route to ensure passthrough works
-ip route add default via 192.168.2.1 dev eth0
+sudo ip route add default via 192.168.2.1 dev eth0
