5800873d12110ba2b6057fb98f42998942256bb5
Evan Palmer <evanp922@gmail.com>
2026-09-08 17:51:50 -0700

    bump changelogs

--- modules/autonomy_pi/CHANGELOG.md
+++ modules/autonomy_pi/CHANGELOG.md
@@ -1,5 +1,7 @@
 # Changelog for module autonomy_pi
 
+## 0.2.1
+
 ## 0.2.0
 
 - Implements the `autonomy_msgs` package, starting with `ImuCalibrationStatus`.
--- modules/bluerov_pi/scripts/nat.sh
+++ modules/bluerov_pi/scripts/nat.sh
@@ -2,6 +2,3 @@
 
 # configure the network to point to the topside device for internet access
 sudo nmcli device modify eth0 ipv4.addresses 192.168.2.2/24 ipv4.gateway 192.168.2.1 ipv4.dns 8.8.8.8 ipv4.method manual
-
-# force the default route to ensure passthrough works
-sudo ip route add default via 192.168.2.1 dev eth0
